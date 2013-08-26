53119ff885d625ef30e11ebcc20911d1038e6c18
JeffJacobson <jeffrey.m.jacobson@github.fake>
2013-08-26 16:56:49 -0700

    Added files to repository.

---
+++ Scripts/index.js
@@ -0,0 +1,18 @@
+﻿require(["esri/map", "esri/geometry/webMercatorUtils"], function (Map, webMercatorUtils) {
+	var map;
+
+	map = new Map("map", {
+		basemap: "osm"
+	});
+
+	function updateViewbox(evt) {
+		var extent = evt.extent;
+		if (extent) {
+			// Convert the extent to geographic.
+			extent = webMercatorUtils.webMercatorToGeographic(extent);
+			document.querySelector("[name=viewbox]").value = [extent.xmin, extent.ymin, extent.xmax, extent.ymax].join(",");
+		}
+	}
+
+	map.on("extent-change", updateViewbox);
+});
